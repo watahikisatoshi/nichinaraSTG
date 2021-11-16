d690bb62256e1a22dfd613e6d8073f1edbba47e4
watahikisatoshi <cssa21226@g.nihon-u.ac.jp>
2021-11-16 20:43:00 +0900

    test9

--- Assets/Scripts/Bosses/Bosscontroller.cs
+++ Assets/Scripts/Bosses/Bosscontroller.cs
@@ -4,12 +4,19 @@ using UnityEngine;
 
 public class Bosscontroller : MonoBehaviour
 {
+    private float time;
+
     public GameObject BossBullet;
+    public int speed = 5;
+    Vector3 movePosition;
+
 
 
     // Start is called before the first frame update
     void Start()
     {
+        movePosition = moveRandomPosition();
+
         InvokeRepeating("Shot", 2f, 1f);
     }
 
@@ -17,11 +24,31 @@ public class Bosscontroller : MonoBehaviour
     {
 
         Instantiate(BossBullet, transform.position, transform.rotation);
+    }
         // Update is called once per frame
         void Update()
         {
+            
+            if(movePosition == BossBullet.transform.position)
 
+            {
+                movePosition = moveRandomPosition();
+            }
+
+            this.BossBullet.transform.position = Vector3.MoveTowards(BossBullet.transform.position, movePosition, speed * Time.deltaTime);
 
         }
+
+    private Vector3 moveRandomPosition()
+
+    {
+
+        Vector3 randomPosi = new Vector3(Random.Range(-6,6),Random.Range(-6,6),5);
+        return randomPosi;
+
     }
+
+        
+
+    
 }
